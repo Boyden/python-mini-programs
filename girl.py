425476457611046f4ebbda30f39442cf91fdf365
boyden <lee787359142@gmail.com>
2017-07-06 22:57:28 +0800

    Add files via upload

---
+++ girl.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os, imageio
+
+gifFileName = "‪C:\\Users\\acer\\Desktop\\girl.gif"
+path = gifFileName[:-4]
+os.mkdir(path)
+gif = Image.open(gifFileName)
+try:
+	while True:
+		current = gif.tell()
+		gif.save(path + "\\" + str(current) + ".png")
+		gif.seek(current + 1)
+except EOFError:
+	pass
+
+pathlist = os.listdir(path)
+im = []
+images = []
+
+for p in pathlist:
+    newPath = os.path.join(path, p)
+    im.append(Image.open(newPath).convert("L"))
+    im[pathlist.index(p)] = im[pathlist.index(p)].point(lambda i: i > 128 and 255)
+    im[pathlist.index(p)].save(newPath, "PNG")
+    images.append(imageio.imread(newPath))
+
+imageio.mimsave(path + '//girl.gif', images)
