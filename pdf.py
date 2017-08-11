872f9c74ebe10aeea9c296100204de0ffa1eac24
boyden <lee787359142@gmail.com>
2017-08-11 17:58:58 +0800

    Add files via upload

---
+++ pdf.py
@@ -0,0 +1,18 @@
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+def merge_pdf(infnList, outfn):
+    pdf_output = PdfFileWriter()
+    for infn in infnList:
+        pdf_input = PdfFileReader(open(infn, 'rb'))
+        # 获取 pdf 共用多少页
+        page_count = pdf_input.getNumPages()
+        print(page_count)
+        for i in range(page_count):
+            pdf_output.addPage(pdf_input.getPage(i))
+    pdf_output.write(open(outfn, 'wb'))
+
+path = "C:\\Users\\cole\\Desktop\\PDF"
+os.chdir(path)
+infnList = os.listdir(path)
+merge_pdf(infnList, "C:\\Users\\cole\\Desktop\\linear_algebra.pdf")
