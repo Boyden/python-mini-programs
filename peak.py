bb51fe120ffbfdb3b19e7a6b0c12932fbc945255
boyden <lee787359142@gmail.com>
2017-10-29 17:18:14 +0800

    Add files via upload

---
+++ peak.py
@@ -0,0 +1,34 @@
+import math
+def fun(x):
+	return x*math.cos(x)
+
+def fun(x):
+	return 10*x + 70/x
+
+def max_pole(arr, delta, num):
+	x1 = arr[0]
+	x2 = arr[1]
+	for i in range(num):
+		if fun((x1+x2-delta)/2) < fun((x1+x2+delta)/2):
+			print(str((x1+x2-delta)/2) + " : "+str(fun((x1+x2-delta)/2)))
+			print(str((x1+x2+delta)/2) + " : "+str(fun((x1+x2+delta)/2)))
+			x1 = (x1+x2-delta)/2
+		else:
+			print(str((x1+x2-delta)/2) + " : "+str(fun((x1+x2-delta)/2)))
+			print(str((x1+x2+delta)/2) + " : "+str(fun((x1+x2+delta)/2)))
+			x2 = (x1+x2+delta)/2
+		print([x1, x2])
+
+def min_pole(arr, delta, num):
+	x1 = arr[0]
+	x2 = arr[1]
+	for i in range(num):
+		if fun((x1+x2-delta)/2) < fun((x1+x2+delta)/2):
+			print(str((x1+x2-delta)/2) + " : "+str(fun((x1+x2-delta)/2)))
+			print(str((x1+x2+delta)/2) + " : "+str(fun((x1+x2+delta)/2)))
+			x2 = (x1+x2+delta)/2
+		else:
+			print(str((x1+x2-delta)/2) + " : "+str(fun((x1+x2-delta)/2)))
+			print(str((x1+x2+delta)/2) + " : "+str(fun((x1+x2+delta)/2)))
+			x1 = (x1+x2-delta)/2
+		print([x1, x2])
