470ca5aed30442645237c8f0d6d66a7bc0aae231
boyden <lee787359142@gmail.com>
2017-11-03 18:21:57 +0800

    Add files via upload

---
+++ fft.py
@@ -0,0 +1,50 @@
+import math, time
+N = 1024
+
+def W(N, r):
+    return math.e**(-2*r*math.pi*1j/N)
+
+def swap(li, i, j):
+    c = li[i]
+    li[i] = li[j]
+    li[j] = c
+
+def symme(a, N):
+    s = str(bin(a))[2:]
+    s = (int(math.log2(N)) - len(s))*"0" + s
+    s = s[::-1]
+    return int(''.join(s), 2)
+
+def cal(li, m):
+    x = y = [None for i in range(len(li))]
+    for i in range(len(li)):
+        if i in x:
+            x[i] = i - 2**m
+        else:
+            x[i] = i + 2**m
+
+    for i in range(len(li)):
+        if i < x[i]:
+            y[i] = li[i] + W(2**(m+1), i%(2**m))*li[x[i]]
+        else:
+            y[i] = li[x[i]] - W(2**(m+1), i%(2**m))*li[i]
+    return y
+
+def fft(li, N = 2**(int(math.log2(len(li))) + 1)):
+    y = []
+    if len(li) < N:
+        temp = [0 for i in range(N - len(li))]
+        li = li + temp
+    for i in range(len(li)):
+        y.append(li[symme(i, N)])
+    for i in range(int(math.log2(N))):
+        if i == 0:
+            li = cal(y, i)
+        else:
+            li = cal(li, i)
+    return li
+
+start = time.time()
+li = [i for i in range(1024)]
+y = fft(li, 1024)
+end = time.time() - start
