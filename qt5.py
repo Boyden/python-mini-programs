a3a034f8b0178cc14f01adfcdc2f5fde2fb180d1
boyden <lee787359142@gmail.com>
2020-05-12 11:14:24 +0800

    update qt5

--- qt5.py
+++ qt5.py
@@ -1,9 +1,9 @@
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QFileDialog, QApplication, QWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QFileDialog, QApplication, QWidget, QHBoxLayout, QVBoxLayout, QTableWidget
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
-class Example(QWidget):
+class Train_window(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -13,32 +13,32 @@ class Example(QWidget):
     def initUI(self):      
 
         self.pushButton = QtWidgets.QPushButton(self)
-        self.pushButton.setGeometry(QtCore.QRect(290, 300, 100, 50))
-        self.pushButton.setObjectName("pushButton")
         self.lineEdit = QtWidgets.QLineEdit(self)
-        self.lineEdit.setGeometry(QtCore.QRect(230, 240, 100, 40))
-        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton_1 = QtWidgets.QPushButton(self)
         self.pushButton_2 = QtWidgets.QPushButton(self)
-        self.pushButton_2.setGeometry(QtCore.QRect(230, 170, 100, 50))
-        self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_3 = QtWidgets.QPushButton(self)
-        self.pushButton_3.setGeometry(QtCore.QRect(360, 170, 100, 50))
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.lineEdit_1 = QtWidgets.QLineEdit(self)
         self.lineEdit_2 = QtWidgets.QLineEdit(self)
-        self.lineEdit_2.setGeometry(QtCore.QRect(360, 240, 100, 40))
-        self.lineEdit_2.setObjectName("lineEdit_2")
-
+        
         self.pushButton.setText("确认")
+        self.pushButton_1.setText("类别数")
         self.pushButton_2.setText("样本数")
         self.pushButton_3.setText("维度")
+        self.lineEdit.setText("10")
+        self.lineEdit_1.setText("2")
+        self.lineEdit_2.setText("2")
+
+        self.pushButton.clicked.connect(self.gen_table)
 
         hbox = QHBoxLayout()
         hbox.addWidget(self.pushButton_2)
         hbox.addWidget(self.pushButton_3)
+        hbox.addWidget(self.pushButton_1)
 
         h_line = QHBoxLayout()
         h_line.addWidget(self.lineEdit)
         h_line.addWidget(self.lineEdit_2)
+        h_line.addWidget(self.lineEdit_1)
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
@@ -47,12 +47,50 @@ class Example(QWidget):
 
         self.setLayout(vbox)
         self.setGeometry(300, 300, 500, 500)
-        self.setWindowTitle('QCheckBox')
+        self.setWindowTitle('Train data')
         self.show()
-   
+
+    def gen_table(self):
+        self.hide()
+        col = int(self.lineEdit_1.text()) + 1
+        row = int(self.lineEdit.text()) + 1
+        print('row:{}, col:{}'.format(row, col))
+        train_data = Train_data(row, col)
+        train_data.show()
+
+class Train_data(QWidget):
+
+    def __init__(self, row, col):
+        super().__init__()
+
+        self.initUI(row, col)
+
+    def initUI(self, row, col):      
+
+        self.table = QTableWidget()
+        self.table.setRowCount(row)
+        self.table.setColumnCount(col)
+
+        # hbox = QHBoxLayout()
+        # hbox.addWidget(self.pushButton_2)
+        # hbox.addWidget(self.pushButton_3)
+
+        # h_line = QHBoxLayout()
+        # h_line.addWidget(self.lineEdit)
+        # h_line.addWidget(self.lineEdit_2)
+
+        # vbox = QVBoxLayout()
+        # vbox.addLayout(hbox)
+        # vbox.addLayout(h_line)
+        # vbox.addWidget(self.pushButton)
+
+        # self.setLayout(vbox)
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('Train data')
+        # self.show()
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = Train_window()
     sys.exit(app.exec_())
